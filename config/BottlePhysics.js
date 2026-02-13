847fea0adde6ace6205f7aae10e4c4314cbdb497
GitCoder0502023 <K.alam93899@gmail.com>
2026-02-13 22:34:02 +0530

    refactor(repo): remove unused constants and config files

--- config/BottlePhysics.js
+++
@@ -1,8 +0,0 @@
-// Bottle Physics (Hidden from player mostly)
-export let bottle = {
-    lockedDir: 'CW', // or 'ACW'
-    requiredForce: 0,
-    maxCapacity: 0,  // If force > this, bottle breaks
-    currentTightness: 0, // Errors add to this
-    isOpen: false
-};
--- config/rl.js
+++
@@ -1,7 +0,0 @@
-import readline from 'readline';
-
-// --- INPUT HANDLER ---
-export const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
